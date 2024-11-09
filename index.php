e1767b02fe95b7306ef904350b1048e3a128e010
aziz zaaine <zaaine@live.fr>
2024-11-09 10:51:05 +0100

    Gestion des erreurs et ajout de 2 classes Exception.

--- index.php
+++ index.php
@@ -3,20 +3,37 @@
 
 declare(strict_types=1);
 
-spl_autoload_register(static function (string $fqcn): void {
-    $path = sprintf('%s.php', str_replace(['App', '\\'], ['src', '/'], $fqcn));
+spl_autoload_register(static function ($fqcn): void {
+    $path = sprintf('%s.php', str_replace(['App\\Domain', '\\'], ['src', '/'], $fqcn));
     require_once $path;
 });
 
-use App\MatchMaker\Lobby;
-use App\MatchMaker\Player\Player;
+use App\Domain\MatchMaker\Encounter\Score;
+use App\Domain\MatchMaker\Lobby;
+use App\Domain\MatchMaker\Player\Player;
 
 $greg = new Player('greg');
-$jade = new Player('jade');
+$chuckNorris = new Player('Chuck Norris', 3000);
 
 $lobby = new Lobby();
-$lobby->addPlayers($greg, $jade);
+$lobby->addPlayer($greg);
+$lobby->addPlayer($chuckNorris);
 
-var_dump($lobby->findOponents($lobby->queuingPlayers[0]));
+while (count($lobby->queuingPlayers)) {
+    $lobby->createEncounters();
+}
 
-exit(0);
+$encounter = end($lobby->encounters);
+
+// ces scores sont fictifs !
+$encounter->setScores(
+    new Score(score: 42, player: $greg),
+    new Score(score: 1, player: $chuckNorris)
+);
+
+var_dump($encounter);
+
+$encounter->updateRatios();
+
+var_dump($greg);
+var_dump($chuckNorris);
---
+++ src/Exceptions/NotEnoughPlayersException.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Domain\Exceptions;
+
+class NotEnoughPlayersException extends \RuntimeException
+{
+}
